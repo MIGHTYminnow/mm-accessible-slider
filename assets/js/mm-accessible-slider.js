50dc3d15810d58fbd2d1cc41d2446acc50df2852
Aaron Barraza <aaron@mightyminnow.com>
2020-01-13 12:55:24 -0700

    Add back images to navigation.

--- assets/js/mm-accessible-slider.js
+++ assets/js/mm-accessible-slider.js
@@ -211,11 +211,13 @@ var myCarousel = (function() {
 		if(settings.slidenav) {
 			var buttons = carousel.querySelectorAll('.slidenav button[data-slide]');
 			for (var j = buttons.length - 1; j >= 0; j--) {
+				var thumb = jQuery( '.mm-accessible-slider .slide' ).eq( j ).data( 'thumb' );
 				buttons[j].className = '';
-				buttons[j].innerHTML = '<span class="visuallyhidden">News</span> ' + (j+1);
+				buttons[j].innerHTML = '<span class="visuallyhidden">Slide ' + (j+1) + '</span> <img src="' + thumb + '" alt="Slide ' + (j+1) + '">';
 			}
+			var thumb = jQuery( '.mm-accessible-slider .slide' ).eq( new_current ).data( 'thumb' );
 			buttons[new_current].className = "current";
-			buttons[new_current].innerHTML = '<span class="visuallyhidden">News</span> ' + (new_current+1) + ' <span class="visuallyhidden">(Current Item)</span>';
+			buttons[new_current].innerHTML = '<span class="visuallyhidden">Slide ' + (new_current+1) + ' (Current Item)</span> <img src="' + thumb + '" alt="Slide ' + (j+1) + '">';
 		}
 
 		index = new_current;
