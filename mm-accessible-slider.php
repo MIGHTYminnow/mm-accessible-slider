aca2b2ee373e21cfc33923884e91e7fc9a48a162
Aaron Barraza <aaron@mightyminnow.com>
2020-01-17 10:48:25 -0700

    [TAG] Version 1.1.0

--- mm-accessible-slider.php
+++ mm-accessible-slider.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: MM Accessible Slider
  * Description: Accessible Slider by MIGHTYminnow.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: MIGHTYminnow
  * Author URI: https://mightyminnow.com
  */
