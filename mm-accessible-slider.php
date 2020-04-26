b82627c970d88f7f02d2f4508d264a1a3ffd6b65
Aaron Barraza <hola@aaronbarraza.mx>
2020-04-26 15:31:19 -0700

    Remove files and code that won't be used on v2.

--- mm-accessible-slider.php
+++ mm-accessible-slider.php
@@ -9,7 +9,3 @@
 
 define( 'MM_ACCESSIBLE_SLIDER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'MM_ACCESSIBLE_SLIDER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-
-require_once( MM_ACCESSIBLE_SLIDER_PLUGIN_DIR . 'includes/class-mm-accessible-slider.php' );
-
-MM_Accessible_Slider::init();
